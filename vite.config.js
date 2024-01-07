8f8406a03bc9f7c68557e16b21d99c65d18a1d65
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-07 00:47:32 -0800

    init

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-complete-guide-course/08-Working-with-Refs-Portals/",
+  base: "/the-almost-final-countdown/",
 });
